3dd5122e1736758769ed7f300d6a3e1cbbc1ed14
bongofriend <memiaslan1905@gmail.com>
2017-11-27 15:31:37 +0100

    Fix Port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require("passport");
 
 const app = express();
 const router = express.Router();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Set up middleware
 app.use(passport.initialize());
