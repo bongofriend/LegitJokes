fe8930d35e413961a7923de8221a447e645970b1
bongofriend <memiaslan1905@gmail.com>
2017-12-04 21:03:54 +0100

    Add Mongoose Models

---
+++ database/models.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const moment = require("moment");
+
+const userSchema = mongoose.Schema({
+    Username: String,
+    Password: String,
+    Coins: {type: Number, default: 0}
+});
+
+const jokeSchema = mongoose.Schema({
+    Content: String,
+    Date: {type: String, default: moment.utc().format("YYYY-MM-DD")},
+    Username: String,
+    Category: Number,
+    Upvotes: {type: Number, default: 0},
+    Downvotes: {type: Number, default: 0}
+});
+
+const voteSchema = mongoose.Schema({
+    Username: String,
+    JokeID: Number,
+    VoteType: String
+});
+
+const categorySchema = mongoose.Schema({
+    Category: String
+});
+
+userSchema.pre("save",function(next){
+    let user = this;
+    if(!user.isModified("Password")) return next();
+    bcrypt.genSalt(rounds,function(err,salt){
+        if(err) return next(err);
+        bcrypt.hash(user.Password,salt,null,function(err,hash){
+            if(err) return next(err);
+            user.Password = hash;
+            next();
+        })
+    })
+})
+
+userSchema.methods.comparePassword = function(hash){
+    return new Promise((resolve,reject) => {
+        bcrypt.compare(this.Password,hash,null,(err,isMatch) => {
+            if(err) return reject(err);
+            return resolve(isMatch);
+        })
+    })
+}
+
+module.exports = {
+    userSchema: userSchema,
+    jokeSchema: jokeSchema,
+    voteSchema: voteSchema,
+    categorySchema: categorySchema
+}
