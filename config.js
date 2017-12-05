d2f54750f976c7c947a95da174cfa69d69ea0183
bongofriend <memiaslan1905@gmail.com>
2017-12-05 16:29:58 +0100

    Update DB Config

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 exports.dbconfig = {
-    host: "localhost",
-    database: "jokeapi",
+    host: "ds129386.mlab.com:29386",
+    database: "jokedata",
+    user: "Bongofriend",
+    password: "mertos2001"
 };
 
 exports.jwtconfig = {
