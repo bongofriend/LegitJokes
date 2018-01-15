d04f739720491deaeacfae026bbddf2d1955ade1
bongofriend <memiaslan1905@gmail.com>
2018-01-15 11:06:50 +0100

    Add Views *Add login screen *Add home page

---
+++ views/home.js
@@ -0,0 +1,354 @@
+var categries_link = 'https://legitjokes.herokuapp.com/api//categories';
+var random_joke_link = 'https://legitjokes.herokuapp.com/api/joke/random';
+var joke_in_category_link = 'https://legitjokes.herokuapp.com/api/joke?category='; //missing: example_category
+var submit_joke_link = 'https://legitjokes.herokuapp.com/api//joke/submit'; //missig: examplejoke$category=example_category
+var joke_vote_link = 'https://legitjokes.herokuapp.com/api/vote?id='; //missing: example_id(&vote=up or &vote=down)
+var change_coins_link = 'https://legitjokes.herokuapp.com/api/user/coins?type='; //missing: up or down
+
+
+
+//Vue für die linke Spalte in der die Witze stehen
+
+
+
+var lustig = new Vue({
+	el: ".witzspalte",
+	data: {
+		witze: [],
+		token: null,
+		active: true,
+		no_connection_vid: false,
+	},
+
+	mounted: function(){
+
+		//Speichert Token vom Benutzer
+
+		console.log(localStorage.getItem("token"))
+		console.log(localStorage.getItem("coins"))
+		this.$data.token = "Bearer "
+
+				this.$data.token += localStorage.getItem("token"); //hier Key von Amir einfügen!!
+				//this.$data.token += "eyJhbGciOiJIUzI1NiJ9.TGl0X2JveTY5.EM2R45WtYCgJrIe0zcNPg9yStoEsSwEHudxWA9NlaB8"; //noch meiner
+
+			},
+
+			methods:{
+
+				//Wenn ein Witz Positiv bewertet wird
+
+				upvote(witz){
+					this.$http.get(joke_vote_link + witz.JokeID + '&vote=up',
+					{
+						headers: {
+							'Authorization': this.$data.token,
+						}
+					})
+					.then(function(resp){
+
+
+						if(resp.body.Status == "Ok"){
+							this.$refs.coin_sound.play();
+							witz.Upvotes += 1;
+
+						//Um die Anzahl der Coins für einen User zu erhöhen für die Bewertung
+
+						automat.$data.coins += 1;
+						this.$http.get(change_coins_link + 'up',
+						{
+							headers: {
+								'Authorization': this.$data.token,
+
+							}
+						})
+						.then(function(resp){
+							
+						})
+						.catch(function(err){
+							this.$refs.fail_sound.play();
+						})
+
+
+						 
+
+					}
+
+				})
+					.catch(function(err){
+						this.$refs.fail_sound.play();
+						
+					})
+
+				},
+
+				//Wenn der Witz negativ bewertet wird
+
+				downvote(witz){
+					this.$http.get(joke_vote_link + witz.JokeID + '&vote=down',
+					{
+						headers: {
+							'Authorization': this.$data.token,
+
+						}
+					})
+					.then(function(resp){
+
+						if(resp.body.Status == "Ok"){
+							this.$refs.coin_sound.play();
+
+							witz.Downvotes += 1;
+
+						//Um die Anzahl der Coins für einen User zu erhöhen für die Bewertung
+
+						automat.$data.coins += 1;
+						this.$http.get(change_coins_link + 'up',
+						{
+							headers: {
+								'Authorization': this.$data.token,
+
+							}
+						})
+						.then(function(resp){
+							
+						})
+						.catch(function(err){
+							this.$refs.fail_sound.play();
+						})
+
+
+						
+
+					}
+				})
+					.catch(function(err){
+						this.$refs.fail_sound.play();
+						
+					})
+				},	
+			},			
+		})
+
+
+//Vue für die Navigationsleiste
+
+var category = new Vue({
+	el: ".navigation",
+
+	data: {
+		categories: [], 
+		rechteSeite: true,
+	},
+
+	//Kategorien werden vom Datenbank geladen
+
+	mounted: function() {
+		this.$http.get(categries_link)
+		.then(function(resp) {
+			this.$data.categories = resp.body.data;
+			automat.$data.categories = resp.body.data;
+		})
+		.catch(function(err) {
+			this.$data.categories[0].Category = automat.$data.categories[0].Category = "Da ist wohl";
+			this.$data.categories[1].Category = automat.$data.categories[1].Category = "wer über das";
+			this.$data.categories[2].Category = automat.$data.categories[2].Category = "WLAN-Kabel";
+			this.$data.categories[3].Category = automat.$data.categories[3].Category = "gestolpert";
+		})		
+	},
+	methods: {
+
+		show_us(){
+			lustig.$data.active = true;
+		},
+
+		//Wechsel zum Feld "Witz schreiben" auf der Rechten seite beim Klick auf den Button in der Menüleiste
+
+		switching(){
+			automat.$data.rechteSeite = false;
+			this.$data.rechteSeite = false;
+
+		},
+
+		//Wechsel zurück auf den Automaten durck Klicken auf den Button in der Menüleiste
+
+		back_switching(){
+			automat.$data.rechteSeite = true;
+			this.$data.rechteSeite = true;
+
+
+		},
+
+		//Wechselt in die jeweilige Kategorie druch klicken auf das entsprechende Feld!
+
+		wechsel(id){
+
+
+			this.$http.get(joke_in_category_link + id )
+			.then(function(resp) {
+
+				lustig.$data.witze = resp.body.data;
+				lustig.$data.active = false;
+				lustig.$data.no_connection_vid = false;
+
+			})
+			.catch(function(resp){
+
+				//witze entfernen und video anzeigen das man keine Verbindung hat
+				lustig.$data.no_connection_vid = true;
+				lustig.$data.active = false;
+				lustig.$data.witze = null;
+
+			})
+		}
+
+	}
+
+
+
+
+})
+
+var trial_counter = 1;
+
+//Vue für den Automaten und Textfeld auf der rechten Seite
+
+var automat = new Vue({
+	el: ".rechts",
+
+	data: {
+		zeit: 0,
+		coins: 0, //anpassen
+		random_witz: "Schau in einen Spiegel, da kannst du auch lachen",
+		rechteSeite: true,
+		categories: [],
+		joke: "",
+		selected_category: "Kategorie auswählen",
+		active: false,
+		letter_movement: false,
+	},
+
+
+	mounted: function(){
+
+		//this.$data.coins =  3;
+		localStorage.getItem("coins"); //Coins von Amir
+
+	},
+
+	
+	methods: {
+
+		//Zufälliger Witz im Automaten wird von Datenbank ausgewählt
+
+
+		zufall(){
+			//movement of the machine
+			this.$data.zeit = 1;
+
+			if(this.$data.coins == 1){
+				ruecksetzung_short();
+			} else {
+				ruecksetzung_long();
+			}
+			
+
+			//check if the user has enough coins
+			if(this.$data.coins > 0){
+
+				this.$http.get(random_joke_link)
+				.then(function(resp) {
+      				//show the joke on the screen
+      				this.$data.random_witz = resp.body.data.Content
+      				//decrease coin on screen
+      				this.$data.coins -= 1;
+      				//decrease coin in database
+      				this.$http.get(change_coins_link + 'down',{
+      					headers: {
+      						'Authorization': lustig.$data.token,
+      					}
+      				})
+					localStorage.setItem('coins', 'this.$data.coins');
+				})
+
+				//Failure
+				.catch(function(err) {
+					this.$data.random_witz = "Something went wrong: " + err
+				})		
+			}
+		},
+
+		submit_joke(){
+
+			if(this.selected_category == "Kategorie auswählen"){
+				this.$data.active = true;
+				setTimeout(function(){automat.$data.active = false; }, 500);
+
+			}
+			else {
+				if(this.$data.joke.length <= 15 ){ //if the "joke" is too short
+					if(trial_counter == 1){
+						trial_counter += 1;
+						this.$data.joke = "Das ist kein Witz";
+					} else{
+						this.$data.joke = "IMMER NOCH KEIN WITZ!!!";
+					}
+
+				} else{
+
+				
+
+					this.$http.post(submit_joke_link,
+					{
+						content: this.$data.joke,
+						category: this.$data.selected_category,
+
+					},{
+						headers: {
+							'Authorization': lustig.$data.token,
+						}
+					}
+					)
+					.then(function(resp){
+						this.$data.joke = "";
+						this.$data.letter_movement = true;
+						setTimeout(function(){
+							automat.$data.letter_movement = false;
+						}, 1200);
+						setTimeout(function(){
+						}, 1500);
+
+
+					})
+					.catch(function(err){
+					alert("No Connection to the server :(");
+
+					})
+
+				}
+			}
+
+
+		},
+	}
+
+});
+
+
+var timer;
+
+//Der Timer für die Zeit in der der Automat seinen Hebel nach unten bewegt!
+
+function ruecksetzung_long() {
+	timer = setTimeout(change_back, 1300);
+}
+
+function ruecksetzung_short() {
+	timer = setTimeout(change_back, 1000);
+}
+
+//Die Zeit wird zurück gesetz, damit der Automat sich nicht bewegt!
+
+function change_back(){
+	automat.$data.zeit = 0;
+}
+
+
---
+++ views/index.js
@@ -0,0 +1,294 @@
+// Session-Storage Elemente
+localStorage.setItem('coins', coins);
+localStorage.setItem('token', token);
+
+//Host-Adressen und TOKEN,COINS variable
+
+var hostlog = "https://legitjokes.herokuapp.com/api/user/authenticate";
+var hostreg = "https://legitjokes.herokuapp.com/api/user/register";
+var token;
+var coins;
+
+//Log-in Request
+
+var loginform = new Vue({
+
+
+    el: "#loginform",
+    data: {
+
+       seen:false,
+       username:"",
+       password:"",
+       RegisterLink:"Don't have an Account? Click here to Register",
+       ErrorMessage:"Username or password is wrong!",
+       AnnoyingMessage:"",
+       fails: false,
+       failcounterLogin: 0
+
+   },
+
+
+   methods: {
+
+login: function(e){ 
+    
+   e.preventDefault();
+    console.log("login")
+    this.$http.post(hostlog,{username: this.username,password: this.password})
+    .then(function(res){
+
+      if(res.body.Status === "Ok") 
+      {
+
+         failcounterLogin = 0;
+         this.$data.fails = false;
+         console.log("Success");
+         token = res.body.token;  
+         coins = res.body.coins;
+         console.log("Token: " +token);
+         console.log("Coins: "+ coins);
+         localStorage.setItem('coins', coins);
+         localStorage.setItem('token', token);
+         window.location.replace("home.html");
+
+     }
+     else if(res.body.Status === "Error") 
+     {
+
+        this.$data.failcounterLogin++;
+
+        console.log("Number of Login fails: "+this.failcounterLogin);
+
+        if(this.$data.failcounterLogin>1){
+      
+           this.$data.fails = true;
+
+        }
+
+        switch(this.$data.failcounterLogin){
+          
+          case 2: 
+          this.$data.AnnoyingMessage = "Whats wrong with you ?"
+           break;
+
+          case 3: 
+          this.$data.AnnoyingMessage = "If you don't have an Account click the link !!!"
+           break;
+
+          case 4: 
+          this.$data.AnnoyingMessage = "Do you think this is funny ?"
+           break;
+
+
+          case 5: 
+          this.$data.AnnoyingMessage = "OK, I have nothing to say anymore"
+           break;
+
+          default:
+          this.$data.AnnoyingMessage = ""
+
+
+        }
+
+        this.username = "";
+        this.password = "";
+        this.$data.seen = true;
+
+    }
+
+})
+
+},
+
+
+toRegisterForm: function(){
+
+this.$data.seen = false;
+this.$data.failcounterLogin = 0;
+this.$data.fails = false;
+
+}
+
+}
+
+
+});
+
+
+//Account-Registrieren Register-Request
+
+var registerform = new Vue ({
+
+
+    el: "#RegisterVue",
+    data: {
+
+       regdiv:true,
+       seen: false,
+       username:"",
+       password:"" ,
+       RegHeading:"Create an Account",
+       ErrorMessage:"Name is already used take another one !",
+       success: false
+
+   },
+
+
+
+   methods: {
+
+//AJAX REQUEST
+
+    register: function(e){ 
+        
+       e.preventDefault();
+
+
+        this.$http.post(hostreg,{username: this.username,password: this.password})
+        .then(function(res){
+
+            if(res.body.Status === "Ok"){
+                
+                console.log("Success");
+                token = res.body.token;  
+                coins = res.body.coins;
+                localStorage.setItem('coins', coins);
+                localStorage.setItem('token', token);
+                if(this.$data.seen = true){
+
+                  this.$data.seen = false; }
+
+                  $("#register-div").animate({opacity: 0}, {duration: 500, queue:false});
+                  
+               setTimeout(()=>{
+                  this.$data.success= true;},700);
+
+               setTimeout(()=>{
+                  this.$data.success= false;},3300);
+
+               setTimeout(()=>{
+                 window.location.replace("home.html")},3500);
+
+               }      
+                
+                if(res.body.Status === "Error"){
+
+                   this.$data.seen = true;                
+                   this.username = "";
+                   this.password = "";
+                   console.log("Error");
+
+               }})          
+    },
+
+   // VON REGISTER ZURÜCK ZU LOGIN FORM 
+
+    backToLogin: function(){
+
+       this.$data.seen = false;
+
+       
+
+       $("#register-div").animate({opacity: 0}, {duration: 500, queue:false});
+       
+       setTimeout(function(){
+           $(".form").fadeIn(1000);},1000);
+
+}
+}
+});
+
+
+// VUE für ANFANG 
+
+var firstVue = new Vue({
+
+ el: "#start",
+
+ data:{
+
+active: true,
+hidden:true,
+hiddenbtn:true,
+loginbtn:true,
+menubutton:true,
+
+ },
+
+ mounted: function(){
+
+
+    setTimeout(function(){
+         firstVue.$data.hidden = false;
+     },500);
+
+
+     setTimeout(function(){
+         firstVue.$data.hiddenbtn = false;
+     },3000);
+
+ },
+
+
+
+ methods:{
+  
+  //VERSCHWINDEN DES STARTBILDSCHIRMS UND ERSCHEINEN DER LOGINFORM
+
+     loginform: function(){
+
+        firstVue.$data.menubutton = false;
+
+        setTimeout(function(){
+            $("ul").fadeToggle("slow");
+        },1000);
+
+        setTimeout(function(){
+            $(".form").css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 'slow')
+        },2000);
+
+        firstVue.$data.hidden = true;
+     
+        firstVue.$data.hiddenbtn = true;
+
+    }
+    
+
+
+}
+
+
+});
+
+
+
+
+// JQUERY für den Wechsel von LOGIN zu REGISTER 
+
+
+$(document).ready(function(){
+
+
+    $("#toRegisterForm").click(function(){
+
+        $('.form').fadeOut(1000)
+
+        setTimeout(function(){
+            $("#register-div").animate({opacity: 1}, {duration: 500, queue:false});
+
+        },1500);
+
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
